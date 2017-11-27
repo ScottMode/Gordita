2c899e82005ec4925dbebf4fc6dabf922b09b82e
Nelson Scott <nelson@scottmode.com>
2017-11-27 08:30:15 -0500

    Testing for build

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -6,7 +6,7 @@ public class Monster : NetworkBehaviour
 {
 	#region Fields
 	public bool isAttacking;
-	float attackTimer;
+	public float attackTimer;
 	public float attackMaxTime;
 
 	public bool canAttack;
---
+++ Assets/MonsterHUD.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MonsterHUD : MonoBehaviour 
+{
+
+	public Slider slider;
+
+	void Update()
+	{
+		slider.value = 1.00f - (GameManager.Instance.monster.attackTimer / GameManager.Instance.monster.attackMaxTime);
+	}
+}
